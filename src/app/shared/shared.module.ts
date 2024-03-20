7d0d1d5c1adf45684d1c066dabfcabff974beca2
Ahmad <ahmads20002000@gmail.com>
2024-03-20 07:51:50 +0200

    Add navbar component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [HeaderComponent, SpinnerComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     exports: [HeaderComponent, SpinnerComponent],
 })
 export class SharedModule {}
